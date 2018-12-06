af2d7ff7bac1b8a6fb86e43c8601868fdf6f07ca
Yogesh Kanda <yogesh.kanda2@mindtree.com>
2018-12-06 13:56:43 +0530

    Updated cat.api.spec.js , removed unnecessary code/comments from it

--- spec_tests/cat.api.spec.js
+++ spec_tests/cat.api.spec.js
@@ -2,21 +2,12 @@ var file = require('fs'),
 superTest = require('supertest'),
 config = require('config'),
 request = superTest(global.ENV);
-
-
-// extend with Request#proxy()
-require('superagent-proxy')(request);
- 
-// HTTP, HTTPS, or SOCKS proxy to use
-var proxy = process.env.http_proxy || 'http://internetpu:8085';
  
 describe('JEST Demo', ()=>{
     test('It should get 200 status code', (done) =>{
-        mocks.use(['getUsers'])
 
-        request.get('/favourites')
-        .set("x-api-key", "04cf3299-801e-4eac-b899-31c16488f94e")
-        .proxy(proxy)
+        request.get('v1/favourites')
+        .set("X-Api-Key", "04cf3299-801e-4eac-b899-31c16488f94e")
         .end( (err, res) =>{
             if(err) done.fail(err);
             expect(res.status).toBe(200);
