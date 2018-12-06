89226248bdea2353fbe567cbcab6f3e75b3811e9
Yogesh Kanda <yogesh.kanda2@mindtree.com>
2018-12-06 18:29:59 +0530

    added more test cases in cat.api.spec.js

--- spec_tests/cat.api.spec.js
+++ spec_tests/cat.api.spec.js
@@ -3,14 +3,111 @@ superTest = require('supertest'),
 config = require('config'),
 request = superTest(global.ENV);
  
-describe('JEST Demo', ()=>{
-    test('It should get 200 status code', (done) =>{
+var d = new Date();
+this.vote_id = 123;
 
-        request.get('v1/favourites')
+describe('Cat API', ()=>{
+    it('should return 200 status code', (done) =>{
+
+        request.get('v1/breeds')
         .set("X-Api-Key", "04cf3299-801e-4eac-b899-31c16488f94e")
         .end( (err, res) =>{
             if(err) done.fail(err);
             expect(res.status).toBe(200);
+            done();
+        });
+    });
+
+    it('should get a list of cat breeds', (done) =>{
+
+        request.get('v1/breeds')
+        .set("X-Api-Key", "04cf3299-801e-4eac-b899-31c16488f94e")
+        .end( (err, res) =>{
+            if(err) done.fail(err);
+            expect(res.status).toBe(200);
+            console.log(res.headers);
+            done();
+        });
+    });
+
+    it('should return a list of categories', (done) =>{
+
+        request.get('v1/categories')
+        .set({
+            "Content-Type": "application/json",
+            "X-Api-Key" : "04cf3299-801e-4eac-b899-31c16488f94e"
+        })
+        .end( (err, res) =>{
+            if(err) done.fail(err);
+            expect(res.status).toBe(200);
+            console.log(res.headers);
+            // expect(res.headers).to.
+            done();
+        });
+    });
+
+    it('should create a vote for selected image', (done) =>{
+
+        request.post('v1/votes')
+        .send({
+            "image_id": "asf2",
+            "value": 1
+        })
+        .set("X-Api-Key", "04cf3299-801e-4eac-b899-31c16488f94e")
+        .end( (err, res) =>{
+            if(err) done.fail(err);
+            expect(res.status).toBe(200);
+            console.log(res.body);
+            this.vote_id = res.body.id;
+            done();
+        });
+    });
+
+    it('should return votes created by user', (done) =>{
+
+        request.get('v1/votes')
+        .set("X-Api-Key", "04cf3299-801e-4eac-b899-31c16488f94e")
+        .end( (err, res) =>{
+            if(err) done.fail(err);
+            expect(res.status).toBe(200);
+            console.log(res.body);
+            done();
+        });
+    });
+
+    it('should return vote specified', (done) =>{
+        request.get('v1/votes/' + this.vote_id)
+        .set("X-Api-Key", "04cf3299-801e-4eac-b899-31c16488f94e")
+        .end( (err, res) =>{
+            if(err) done.fail(err);
+            expect(res.status).toBe(200);
+            expect(res.body.id).toBe(this.vote_id);
+            done();
+        });
+    });
+
+    it('should delete specified vote', (done) =>{
+        request.delete('v1/votes/' + this.vote_id)
+        .set("X-Api-Key", "04cf3299-801e-4eac-b899-31c16488f94e")
+        .end( (err, res) =>{
+            if(err) done.fail(err);
+            expect(res.status).toBe(200);
+            expect(res.body.message).toBe("SUCCESS");
+            done();
+        });
+    });
+
+    it('should save an image as favourite', (done) =>{
+        vote_id = 47129;
+        request.post('v1/favourites')
+        .send({
+            "image_id": "asf2",
+        })
+        .set("X-Api-Key", "04cf3299-801e-4eac-b899-31c16488f94e")
+        .end( (err, res) =>{
+            if(err) done.fail(err);
+            expect(res.status).toBe(200);
+            expect(res.body.message).toBe("SUCCESS");
             done();
         });
     });
