765126a10208e518090d0ab0dcdfa60ca43361ef
Yogesh Kanda <yogesh.kanda2@mindtree.com>
2018-12-07 19:05:10 +0530

    added more tests

--- spec_tests/cat-api/breeds.cat.api.spec.js
+++ spec_tests/cat-api/breeds.cat.api.spec.js
@@ -5,6 +5,9 @@ mocks = require('../fixtures/http_mocks');
 
 describe('Breeds route', ()=>{
 
+    response_limit = 1;
+    page_limit = 1;
+
     it('should return 200 status code', (done) =>{
 
         mocks.use(['listAllCatBreeds']);
@@ -13,11 +16,28 @@ describe('Breeds route', ()=>{
         .set("X-Api-Key", config.get("app.api-key"))
         .end( (err, res) =>{
             if(err) done.fail(err);
-            console.log(res.status, res.headers);
+            // console.log(res.status, res.headers);
+            // console.log(res.body);
+            expect(res.status).toBe(200);
+            expect()
+            done();
+        });
+    });
+
+    it('should return specified limit of data in response body', (done) =>{
+
+        mocks.use(['listAllCatBreeds']);
+
+        request.get('/v1/breeds?limit=' + response_limit + '&page=' + page_limit)
+        .set("X-Api-Key", config.get("app.api-key"))
+        .end( (err, res) =>{
+            if(err) done.fail(err);
+            // console.log(res.status, res.headers);
             console.log(res.body);
             expect(res.status).toBe(200);
             expect()
             done();
         });
     });
+
 });
--- spec_tests/cat-api/categories.cat.api.spec.js
+++ spec_tests/cat-api/categories.cat.api.spec.js
@@ -5,6 +5,9 @@ mocks = require('../fixtures/http_mocks');
 
 describe('Categories route', ()=>{
 
+    response_limit = 2;
+    page_limit = 1;
+
     it('should return 200 status code', (done) =>{
 
         mocks.use(['listAllCategories']);
@@ -13,11 +16,42 @@ describe('Categories route', ()=>{
         .set("X-Api-Key", config.get("app.api-key"))
         .end( (err, res) =>{
             if(err) done.fail(err);
-            console.log(res.status, res.headers);
-            console.log(res.body);
+            // console.log(res.status, res.headers);
+            // console.log(res.body);
             expect(res.status).toBe(200);
             expect()
             done();
         });
     });
+    
+    it('should return specified limit of data in response body', (done) =>{
+
+
+
+        mocks.use(['listAllCategories']);
+
+        request.get('/v1/categories?limit=' + response_limit + '&page=' + page_limit)
+        .set("X-Api-Key", config.get("app.api-key"))
+        .end( (err, res) =>{
+            
+            if(err) done.fail(err);
+            // console.log(res.status, res.headers);
+            //     res.on('data', function (chunk) {
+            //         // str += chunk;
+            //   });
+
+            //     res.on('end', function () {
+            //         // console.log(str);
+            //   });
+
+            // res.on(res, function(body){
+            //     console.log("inside response.on");
+            //     console.log(body);
+            // });
+            // expect(res.status).toBe(200);
+            // expect()
+            done();
+        });
+    });
+
 });
