83d3c724f212dc02fb6aeca11295c29d4a6540b5
Vishal Swarnkar <vishalswa@gmail.com>
2018-12-07 09:48:13 +0530

    removed the commented code

--- spec_tests/cat-api/cat.api.spec.js
+++ spec_tests/cat-api/cat.api.spec.js
@@ -5,37 +5,37 @@ request = superTest(global.ENV),
 mocks = require('../fixtures/http_mocks');
 
 describe('JEST Demo', ()=>{
-    // test('\/breeds route shall return 200 response', (done) =>{
-    //
-    //     mocks.use(['listAllCatBreeds']);
-    //
-    //     request.get('/v1/breeds')
-    //     .set("X-Api-Key", config.get("app.api-key"))
-    //     .end( (err, res) =>{
-    //         if(err) done.fail(err);
-    //         console.log(res.status, res.headers);
-    //         console.log(res.body);
-    //         expect(res.status).toBe(200);
-    //         expect()
-    //         done();
-    //     });
-    // });
+    test('\/breeds route shall return 200 response', (done) =>{
 
-    // test('\/categories route shall return 200 response', (done) =>{
-    //
-    //     mocks.use(['listAllCategories']);
-    //
-    //     request.get('/v1/categories')
-    //     .set("X-Api-Key", config.get("app.api-key"))
-    //     .end( (err, res) =>{
-    //         if(err) done.fail(err);
-    //         console.log(res.status, res.headers);
-    //         console.log(res.body);
-    //         expect(res.status).toBe(200);
-    //         expect()
-    //         done();
-    //     });
-    // });
+        mocks.use(['listAllCatBreeds']);
+
+        request.get('/v1/breeds')
+        .set("X-Api-Key", config.get("app.api-key"))
+        .end( (err, res) =>{
+            if(err) done.fail(err);
+            console.log(res.status, res.headers);
+            console.log(res.body);
+            expect(res.status).toBe(200);
+            expect()
+            done();
+        });
+    });
+
+    test('\/categories route shall return 200 response', (done) =>{
+
+        mocks.use(['listAllCategories']);
+
+        request.get('/v1/categories')
+        .set("X-Api-Key", config.get("app.api-key"))
+        .end( (err, res) =>{
+            if(err) done.fail(err);
+            console.log(res.status, res.headers);
+            console.log(res.body);
+            expect(res.status).toBe(200);
+            expect()
+            done();
+        });
+    });
     test('\/favourites route shall return 200 response', (done) =>{
 
         mocks.use(['listAllFavourities']);
